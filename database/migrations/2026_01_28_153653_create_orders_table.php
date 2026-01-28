0e20052dac812a0c374d59fa5f8b6478cee07d30
GabMaisonneuve <e2496359@cmaisonneuve.qc.ca>
2026-01-28 10:52:54 -0500

    Création des tables pour migration vers base de donnée

---
+++ database/migrations/2026_01_28_153653_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id')->constrained()->cascadeOnDelete();
+
+            $table->string('customer_name');
+            $table->string('customer_email');
+
+            $table->string('shipping_address');
+            $table->string('shipping_city');
+            $table->string('shipping_state');
+            $table->string('shipping_zip');
+            $table->string('shipping_country');
+
+            $table->decimal('total', 8, 2);
+            $table->string('status')->default('paid');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
